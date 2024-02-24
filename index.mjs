1b8d4a9359c907ef4472f9434506e9583ec9aafe
PC <PC@DESKTOP-0L6LCVP>
2024-02-24 16:01:50 -0300

    Alterando log

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { logCustomizado } from './log.mjs';
 export const handler = async (event) => {
 
-    logCustomizado('Log de execução event: ' + JSON.stringify(event));
+    logCustomizado('Log de execução após GitHub ACtion. event: ' + JSON.stringify(event));
     return {
         statusCode: 200,
         body: JSON.stringify(event),
