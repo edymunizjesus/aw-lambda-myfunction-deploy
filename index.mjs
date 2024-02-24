2c073e77148cb883a0daca46e53903154f7cabb3
PC <PC@DESKTOP-0L6LCVP>
2024-02-24 20:24:04 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,7 +1,30 @@
 import { logCustomizado } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
+
+const s3Client = new S3({ region: 'us-east-1' });
+
+
 export const handler = async (event) => {
 
-    logCustomizado('event: ' + JSON.stringify(event));
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket: Bucket,
+        Key: Key,
+    });
+
+    const mega_byte = 1024 * 1024;
+
+    if(getObjectResult.ContentLength > 1 * mega_byte){
+        logCustomizado('Objeto muito grande!!!');
+        return 'Objeto muito grande!!!';
+    }
+
+    logCustomizado('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
+
+
     return {
         statusCode: 200,
         body: `<html><body>Dados da requisicao ${JSON.stringify(event)} </body></html>`,
